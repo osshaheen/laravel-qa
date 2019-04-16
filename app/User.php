2f2ec2563f540126927add9e69e55e0755a3dac4
Osama shaheen <Eng.shaheen91@hotmail.com>
2019-04-16 15:29:03 +0200

    generate and modify question model

--- app/User.php
+++ app/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App;
-
+use Illuminate\Support\Str;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
